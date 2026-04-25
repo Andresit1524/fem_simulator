f322cfe4ec2585ea5b5f6fed4b400d07fce9ee48
Andrés López <andreslopez01182009@gmail.com>
2026-04-25 14:10:29 -0500

    Mejoras en el campo
    - Tamaño de flecha automático
    - Menos flechas para mas rendimiento

--- simulators/electric_fields/field.gd
+++ simulators/electric_fields/field.gd
@@ -1,8 +1,11 @@
 extends Node
 
-@export var field_step: float = 20.0
 
-var arrow_scene := preload("uid://c6tr3lnbx3704")
+@export var field_step: float = 20.0
+@export var arrow_scene: PackedScene
+@export var settings: VectorDisplaySettings
+
+
 var viewport: Viewport
 var last_camera_transform: Transform2D
 var boundaries: Dictionary[String, Vector2] = {
@@ -10,11 +13,17 @@ var boundaries: Dictionary[String, Vector2] = {
 	"bottom_right": Vector2.ZERO,
 }
 
+
 func _ready() -> void:
 	# Inicializa el viewport y su movimiento
 	viewport = get_viewport()
 	last_camera_transform = viewport.get_canvas_transform()
 	_reset_field()
+
+	# Establece los ajustes para las flechas
+	settings.length_mode = VectorDisplaySettings.LenghtModes.CLAMP
+	settings.max_length = field_step
+
 
 func _process(_delta) -> void:
 	# Verifica el movimiento del viewport y re-renderiza el campo
@@ -23,15 +32,6 @@ func _process(_delta) -> void:
 		last_camera_transform = current_transform
 		_reset_field()
 
-## Elimina o regenera el campo eléctrico de la pantalla
-func toggle_field(activate: bool) -> void:
-	if activate:
-		_get_global_viewport_corners()
-		_instantiate_field()
-		return
-
-	for arrow in get_children():
-		arrow.queue_free()
 
 ## Determina la posición del viewport en el mundo
 func _get_global_viewport_corners() -> void:
@@ -44,13 +44,25 @@ func _get_global_viewport_corners() -> void:
 	boundaries.top_left = transform_inversa * rect_visible.position
 	boundaries.bottom_right = transform_inversa * (rect_visible.position + rect_visible.size)
 
+
+## Elimina o regenera el campo eléctrico de la pantalla
+func toggle_field(activate: bool) -> void:
+	if activate:
+		_get_global_viewport_corners()
+		_instantiate_field()
+		return
+
+	for arrow in get_children():
+		arrow.queue_free()
+
+
 ## Instancia las flechas del campo eléctrico a lo largo de una cuadrícula
 func _instantiate_field() -> void:
 	var arrow_coordinates: Array[Vector2]
 	var current_point := boundaries.top_left
 
 	# Recorre todo el espacio de la camara
-	while true:
+	while current_point.y < boundaries.bottom_right.y:
 		arrow_coordinates.append(current_point)
 
 		current_point.x += field_step
@@ -59,13 +71,14 @@ func _instantiate_field() -> void:
 			current_point.x = boundaries.top_left.x
 			current_point.y += field_step
 
-		if current_point.y > boundaries.bottom_right.y:
-			break
-
+	# Añade los puntos
 	for point in arrow_coordinates:
-		var arrow = arrow_scene.instantiate()
-		arrow.position = point
+		var arrow: Arrow = arrow_scene.instantiate()
 		add_child(arrow)
+
+		arrow.position = point
+		arrow.vd2d.settings = settings
+
 
 ## Resetea el campo
 func _reset_field() -> void:
--- simulators/electric_fields/field_arrow.gd
+++ simulators/electric_fields/field_arrow.gd
@@ -1,12 +1,20 @@
-extends Node2D
+class_name Arrow extends Node2D
 
-@export var point_to_mouse: bool = false ## Las cargas se ven atraidas por el mouse
+
+## Las cargas se ven atraidas por el mouse
+@export var point_to_mouse: bool = false
+
+
+@onready var vd2d: VectorDisplay2D = $VectorDisplay2D
+
 
 var force: Vector2
 
-func _process(_delta):
-	force = Algorithms.net_electric_force(self , position)
 
-	if point_to_mouse:
-		var direction := (get_global_mouse_position() - global_position).normalized()
-		force += direction * 100 # Fuerza de atracción arbitraria
+func _process(_delta):
+	force = Algorithms.net_electric_force(self, position)
+
+	if not point_to_mouse: return
+
+	var direction := (get_global_mouse_position() - global_position).normalized()
+	force += direction * 100 # Fuerza de atracción arbitraria
